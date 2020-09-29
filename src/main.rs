05ac4d9adb29d177de83f93fdbdd4882de5af404
steve wirts <stevewirts@gmail.com>
2020-09-29 09:17:52 -0400

    four done

--- src/main.rs
+++ src/main.rs
@@ -2,11 +2,50 @@ use std::collections::HashMap;
 
 fn main() {
     let max_chars = one("Today is Monday");    
-    println!("the most of a char(first appearing) is '{}', appearing {} times", max_chars.0, max_chars.1);
+    println!("1) the most of a char(first appearing) is '{}', appearing {} times", max_chars.0, max_chars.1);
 
-    let test_two_input = "supracalafragalisticexpealaddcious";
+    let test_two_input = "supracalafragalisticexpealadocious";
     let test_two_output = two(test_two_input);
-    println!("all subsequent duplicates removed of {} is {}", test_two_input, test_two_output);
+    println!("2) all subsequent duplicates removed of '{}' is '{}'", test_two_input, test_two_output);
+
+    let test_three_input = "supracalafragalisticexpealadocious";
+    let test_three_output = three(test_three_input);
+    println!("3) duplicates of '{}' is '{}'", test_three_input, test_three_output);
+
+    let test_four_input_1 = "spain";
+    let test_four_input_2 = "the rain in spain falls mainly in the plain";
+    let test_four_output = four(test_four_input_1, test_four_input_2);
+    println!("4) characters of '{}' removed from '{}' yields '{}'", test_four_input_1, test_four_input_2, test_four_output);
+
+}
+
+fn four(input1: &str, input2: &str) -> String {
+    let mut r = String::new();
+    let mut p = 'z';
+    input2.chars().for_each( |c | {
+        if !input1.contains(c) {
+            if !(c == ' ' && p == ' ') {
+                r.push(c);
+                p = c;
+            }
+        }
+    });
+    r
+}
+
+fn three(input: &str) -> String {
+    let mut r = String::new();
+    let mut s = std::collections::HashSet::new();
+    input.chars().for_each( |c | {
+        if !s.contains(&c) {
+            s.insert(c);
+        } else {
+            if !r.contains(c) {
+                r.push(c);
+            }
+        }
+    });
+    r
 }
 
 fn two(input: &str) -> String {
